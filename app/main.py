546d4f16788e0290c71cc6ef88e767eb16413169
steveAzo <stephenazongo77@gmail.com>
2025-10-04 14:33:45 +0000

    allowed cors

--- app/main.py
+++ app/main.py
@@ -8,10 +8,15 @@ from app.workers.schedular import start_scheduler
 app = FastAPI(title="AirSafe Ghana API")
 
 # CORS for frontend
+origins = [
+    "http://localhost:5174", 
+    "http://127.0.0.1:5174",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
+    allow_origins=origins,
+    allow_credentials=True,  
     allow_methods=["*"],
     allow_headers=["*"],
 )
